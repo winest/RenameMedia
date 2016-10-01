a99c6ef3f1f851f423a9966ce4ae610f293b8515
winest <winestwinest@gmail.com>
2016-10-01 22:41:15 +0800

    [RenameMedia] 1. Support more generic filename format 2. Check whether file is already renamed before

--- RenameMedia.py
+++ RenameMedia.py
@@ -12,14 +12,22 @@ import exifread
 #BaseName = 333
 #ExtName = .444
 
-#2015:12:31 00:12:34-My-Description
-g_reExifTime = re.compile( r"^([0-9]{4}):([0-9]{2}):([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})-?(.*)$" )
 
-#1472373079120-My-Description, FB_IMG_1469184029530-My-Description
-g_reTime = re.compile( r"^(FB_IMG_)?([0-9]{10,13})-?(.*)$" )
 
-#Screenshot_2016-07-31-20-50-59-My-Description
-g_reScreenshotTime = re.compile( r"^.*Screenshot_?([0-9]{4})[-_]([0-9]{2})[-_]([0-9]{2})[-_]([0-9]{2})[-_:]([0-9]{2})[-_:]([0-9]{2})-?(.*)$" )
+#2015:12:31 00:12:34
+g_reExifTime = re.compile( r"^([0-9]{4}):([0-9]{2}):([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})$" )
+
+#My-Description-Screenshot_2016-07-31-20-50-59-My-Description, My-Description-C360_2016-07-31-20-50-59-123-My-Description
+g_reDateTime = re.compile( r"^(.*?)-?(Screenshot|C360)[-_]?([0-9]{4})[ -_:]([0-9]{2})[ -_:]([0-9]{2})[ -_:]([0-9]{2})[ -_:]([0-9]{2})[ -_:]([0-9]{2})([ -_:][0-9]{3})?-?(.*)$" )
+
+#My-Description-1472373079120-My-Description, My-Description-FB_IMG_1469184029530-My-Description
+g_reTimeStamp = re.compile( r"^(.*?)-?(FB_IMG)[-_]?([0-9]{10,13})-?(.*)$" )
+
+
+
+#Our goal is to rename file to the format of YYMMDD_HHMMSS-Description
+#My-Description-20120131_112233-My-Description
+g_reAlreadyRenamed = re.compile( r"^(.*?-)?[0-9]{8}_[0-9]{6}(-.*)?$" )
 
 
 
@@ -39,15 +47,19 @@ def GetNewFileName( aDirName , aBaseName , aExt ) :
 
 
 def TryRenameFile( aFilePath , aDirName , aFileName , aBaseName , aExt ) :
-    return RenameByExif( strPath , strDir , strFileName , strBaseName , strExt ) or \
-           RenameByFileName( strPath , strDir , strFileName , strBaseName , strExt )
+    aryAlreadyRenamed = g_reAlreadyRenamed.match( aBaseName )
+    if ( aryAlreadyRenamed ) :
+        return True
+    else :
+        return RenameByExif( strPath , strDir , strFileName , strBaseName , strExt ) or \
+               RenameByFileName( strPath , strDir , strFileName , strBaseName , strExt )
 
 
 
 def RenameByExif( aFilePath , aDirName , aFileName , aBaseName , aExt ) :
     bRet = False
     
-    # Open image file for reading (binary mode)
+    #Open image file for reading (binary mode)
     with open( aFilePath , "rb" ) as f :
         tags = exifread.process_file( f )
         #for tag in sorted( tags.keys() ):
@@ -60,17 +72,30 @@ def RenameByExif( aFilePath , aDirName , aFileName , aBaseName , aExt ) :
     else :
         print( "EXIF: {}".format(tags["EXIF DateTimeOriginal"].printable) )
         
-        #2015:12:31 00:12:34-My-Description
+        #2015:12:31 00:12:34
         aryExifTime = g_reExifTime.match( tags["EXIF DateTimeOriginal"].printable )
         if ( aryExifTime ) :
+            strNewBaseName = "{}{}{}_{}{}{}".format( aryExifTime.group(1) , aryExifTime.group(2) , aryExifTime.group(3) ,
+                                                     aryExifTime.group(4) , aryExifTime.group(5) , aryExifTime.group(6) )
+            
             #Merge original file name description
-            if aryExifTime.group(7) and len( aryExifTime.group(7) ) > 0 :
-                strNewBaseName = "{}{}{}_{}{}{}-{}".format( aryExifTime.group(1) , aryExifTime.group(2) , aryExifTime.group(3) ,
-                                                            aryExifTime.group(4) , aryExifTime.group(5) , aryExifTime.group(6) , 
-                                                            aryExifTime.group(7) )
-            else :
-                strNewBaseName = "{}{}{}_{}{}{}".format( aryExifTime.group(1) , aryExifTime.group(2) , aryExifTime.group(3) ,
-                                                         aryExifTime.group(4) , aryExifTime.group(5) , aryExifTime.group(6) )
+            for count in range( 1 ) :
+                aryDateTime = g_reDateTime.match( aBaseName )
+                if ( aryDateTime ) :
+                    if aryDateTime.group(10) and len( aryDateTime.group(10) ) > 0 :
+                        strNewBaseName += "-" + aryDateTime.group( 10 )
+                    if aryDateTime.group(1) and len( aryDateTime.group(1) ) > 0 :
+                        strNewBaseName += "-" + aryDateTime.group( 1 )
+                    break
+
+                aryTimeStamp = g_reTimeStamp.match( aBaseName )
+                if ( aryTimeStamp ) :
+                    if aryTimeStamp.group(4) and len( aryTimeStamp.group(4) ) > 0 :
+                        strNewBaseName += "-" + aryTimeStamp.group( 4 )
+                    if aryTimeStamp.group(1) and len( aryTimeStamp.group(1) ) > 0 :
+                        strNewBaseName += "-" + aryTimeStamp.group( 1 )
+                    break
+
             strNewFileName = GetNewFileName( aDirName , strNewBaseName , aExt )
             bRet = True
         else :
@@ -87,35 +112,36 @@ def RenameByFileName( aFilePath , aDirName , aFileName , aBaseName , aExt ) :
     bRet = False
     
     for count in range( 1 ) :
-        #1472373079120-My-Description
-        aryTime = g_reTime.match( aBaseName )
-        if ( aryTime ) :
-            #Convert timestamp to specific format
-            strTime = aryTime.group( 2 )
-            if ( len(strTime) > 10 ) :
-                strTime = strTime[:10]
-            strTime = datetime.datetime.fromtimestamp( int(strTime) ).strftime( "%Y%m%d_%H%M%S" )
-            
+        #My-Description-C360_2016-07-31-20-50-59-123-My-Description
+        aryDateTime = g_reDateTime.match( aBaseName )
+        if ( aryDateTime ) :
+            strNewBaseName = "{}{}{}_{}{}{}".format( aryDateTime.group(3) , aryDateTime.group(4) , aryDateTime.group(5) ,
+                                                     aryDateTime.group(6) , aryDateTime.group(7) , aryDateTime.group(8) )
             #Merge original file name description
-            if aryTime.group(3) and len ( aryTime.group(3) ) > 0 :
-                strNewBaseName = "{}-{}".format( strTime , aryTime.group(3) )
-            else :
-                strNewBaseName = strTime
+            if aryDateTime.group(10) and len( aryDateTime.group(10) ) > 0 :
+                strNewBaseName += "-" + aryDateTime.group( 10 )
+            if aryDateTime.group(1) and len( aryDateTime.group(1) ) > 0 :
+                strNewBaseName += "-" + aryDateTime.group( 1 )
             strNewFileName = GetNewFileName( aDirName , strNewBaseName , aExt )
             bRet = True
-            break
 
-        #Screenshot_2016-07-31-20-50-59-My-Description
-        aryScreenshotTime = g_reScreenshotTime.match( aBaseName )
-        if ( aryScreenshotTime ) :
+
+
+        #My-Description-FB_IMG_1469184029530-My-Description
+        aryTimeStamp = g_reTimeStamp.match( aBaseName )
+        if ( aryTimeStamp ) :
+            #Convert timestamp to specific format
+            strTime = aryTimeStamp.group( 3 )
+            if ( len(strTime) > 10 ) :
+                strTime = strTime[:10]
+            strNewBaseName = datetime.datetime.fromtimestamp( int(strTime) ).strftime( "%Y%m%d_%H%M%S" )
+            
             #Merge original file name description
-            if aryScreenshotTime.group(7) and len( aryScreenshotTime.group(7) ) > 0 :
-                strNewBaseName = "{}{}{}_{}{}{}-{}".format( aryScreenshotTime.group(1) , aryScreenshotTime.group(2) , aryScreenshotTime.group(3) ,
-                                                            aryScreenshotTime.group(4) , aryScreenshotTime.group(5) , aryScreenshotTime.group(6) ,
-                                                            aryScreenshotTime.group(7) )
-            else :
-                strNewBaseName = "{}{}{}_{}{}{}".format( aryScreenshotTime.group(1) , aryScreenshotTime.group(2) , aryScreenshotTime.group(3) ,
-                                                         aryScreenshotTime.group(4) , aryScreenshotTime.group(5) , aryScreenshotTime.group(6) )
+            if aryTimeStamp.group(4) and len ( aryTimeStamp.group(4) ) > 0 :
+                strNewBaseName += "-" + aryTimeStamp.group( 4 )
+            if aryTimeStamp.group(1) and len ( aryTimeStamp.group(1) ) > 0 :
+                strNewBaseName += "-" + aryTimeStamp.group( 1 )
+
             strNewFileName = GetNewFileName( aDirName , strNewBaseName , aExt )
             bRet = True
             break
