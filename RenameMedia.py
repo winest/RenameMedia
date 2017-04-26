1c0837c47805ce00c639d7f871b47cfb8bed9abd
winest <winestwinest@gmail.com>
2017-04-26 16:07:27 +0800

    [RenameMedia] 1. Integrated MediaInfo to handle all types of files 2. Rename sequence is now based on EXIF->FileName->MediaInfo->Create/LastModify 3. Log folder will save the conversion history for tracing

--- RenameMedia.py
+++ RenameMedia.py
@@ -1,9 +1,11 @@
 import os
 import sys
+import logging
 import fnmatch
 import re
 import datetime
 import exifread
+from pymediainfo import MediaInfo
 
 
 #For C:\\111\\222\\333.444
@@ -12,18 +14,25 @@ import exifread
 #BaseName = 333
 #ExtName = .444
 
+#Ignore if the time we parsed is earlier than this
+g_dateEarliest = datetime.datetime.strptime( "19991231_235959.999" , "%Y%m%d_%H%M%S.%f" )
 
-#20160521-MyDescription-MyDescription
-g_reBaseNameDesc = re.compile( r"^[^\-]*?-(.+)$" )
+#Our goal is to rename file to the format of YYMMDD_HHMMSS-Description
+#My-Description-20120131_112233-My-Description
+g_reAlreadyRenamed = re.compile( r"^(.*?-)?[0-9]{8}_[0-9]{6}(-.*)?$" )
 
-#2015:12:31 00:12:34
-g_reExifTime = re.compile( r"^([0-9]{4}):([0-9]{2}):([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})$" )
 
-#FB_IMG_1469184029530
-g_reFbTime = re.compile( r"^FB_IMG_([0-9]{10,})$" )
 
-#Screenshot_2016-07-31-20-50-59
-g_reScreenshotTime = re.compile( r".*Screenshot_?([0-9]{4})[-_]([0-9]{2})[-_]([0-9]{2})[-_]([0-9]{2})[-_:]([0-9]{2})[-_:]([0-9]{2})" )
+#My-Description-call_17-25-55_IN_0934023893-My-Description
+g_reCallLogInfo = re.compile( r"^(.*?)-?call_([0-9]{2})-([0-9]{2})-([0-9]{2})_(IN|OUT)_([0-9]{7,})-?(.*)$" )
+
+#My-Description-Screenshot_2016-07-31-20-50-59-My-Description, My-Description-C360_2016-07-31-20-50-59-123-My-Description
+g_reDateTime = re.compile( r"^(.*?)-?(Screenshot|C360|ToramOnlineScreenshot)?[-_]?([0-9]{4})[ -_:]([0-9]{2})[ -_:]([0-9]{2})[ -_:]([0-9]{2})[ -_:]([0-9]{2})[ -_:]([0-9]{2})([ -_:][0-9]{3})?-?(.*)$" )
+
+#My-Description-1472373079120-My-Description, My-Description-FB_IMG_1469184029530-My-Description
+g_reTimeStamp = re.compile( r"^(.*?)-?(FB_IMG)?[-_]?([0-9]{10,13})-?(.*)$" )
+
+
 
 
 
@@ -42,93 +51,196 @@ def GetNewFileName( aDirName , aBaseName , aExt ) :
 
 
 
+#Get comments from the original file name
+def GetComments( aFilePath , aDirName , aFileName , aBaseName , aExt ) :
+    strComment = ""
+    for count in range( 1 ) :
+        #My-Description-call_17-25-55_IN_0934023893-My-Description
+        aryCallLogInfo = g_reCallLogInfo.match( aBaseName )
+        if ( aryCallLogInfo ) :
+            if aryCallLogInfo.group(6) and len( aryCallLogInfo.group(6) ) > 0 :
+                strComment += "-" + aryCallLogInfo.group( 6 )
+            if aryCallLogInfo.group(5) and len( aryCallLogInfo.group(5) ) > 0 :
+                strComment += "-" + aryCallLogInfo.group( 5 )
+            if aryCallLogInfo.group(1) and len( aryCallLogInfo.group(1) ) > 0 :
+                strComment += "-" + aryCallLogInfo.group( 1 )
+            if aryCallLogInfo.group(7) and len( aryCallLogInfo.group(7) ) > 0 :
+                strComment += "-" + aryCallLogInfo.group( 7 )
+            break
+
+        #My-Description-C360_2016-07-31-20-50-59-123-My-Description
+        aryDateTime = g_reDateTime.match( aBaseName )
+        if ( aryDateTime ) :
+            if aryDateTime.group(1) and len( aryDateTime.group(1) ) > 0 :
+                strComment += "-" + aryDateTime.group( 1 )
+            if aryDateTime.group(10) and len( aryDateTime.group(10) ) > 0 :
+                strComment += "-" + aryDateTime.group( 10 )
+            break
+
+        #My-Description-FB_IMG_1469184029530-My-Description
+        aryTimeStamp = g_reTimeStamp.match( aBaseName )
+        if ( aryTimeStamp ) :
+            if aryTimeStamp.group(1) and len( aryTimeStamp.group(1) ) > 0 :
+                strComment += "-" + aryTimeStamp.group( 1 )
+            if aryTimeStamp.group(4) and len( aryTimeStamp.group(4) ) > 0 :
+                strComment += "-" + aryTimeStamp.group( 4 )
+            break
+
+    return strComment
+
+
+
 def TryRenameFile( aFilePath , aDirName , aFileName , aBaseName , aExt ) :
-    return RenameByExif( strPath , strDir , strFileName , strBaseName , strExt ) or \
-           RenameByFileName( strPath , strDir , strFileName , strBaseName , strExt )
+    aryAlreadyRenamed = g_reAlreadyRenamed.match( aBaseName )
+    if ( aryAlreadyRenamed ) :
+        return True
+    else :
+        dateFinal = GetTimeByExif( strPath , strDir , strFileName , strBaseName , strExt ) or \
+                    GetTimeByFileName( strPath , strDir , strFileName , strBaseName , strExt ) or \
+                    GetTimeByMediaInfo( strPath , strDir , strFileName , strBaseName , strExt )
+        strNewBaseName = dateFinal.strftime( "%Y%m%d_%H%M%S" )
+
+        strComment = GetComments( strPath , strDir , strFileName , strBaseName , strExt )
+        if len(strComment) > 0 :
+            strNewBaseName += strComment
+        
+        strNewFileName = GetNewFileName( aDirName , strNewBaseName , aExt )
+        logging.info( "{} => {}".format(aFileName , strNewFileName) )
+        #os.rename( aFilePath , aDirName + strNewFileName )
+        return True
 
 
 
-def RenameByExif( aFilePath , aDirName , aFileName , aBaseName , aExt ) :
-    bRet = False
-    
-    # Open image file for reading (binary mode)
+
+
+
+def GetTimeByExif( aFilePath , aDirName , aFileName , aBaseName , aExt ) :
+    dateFinal = None
+
+    #Open image file for reading (binary mode)
     with open( aFilePath , "rb" ) as f :
         tags = exifread.process_file( f )
         #for tag in sorted( tags.keys() ):
         #    if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
-        #        print( "Key: {}, value {}".format(tag, tags[tag]) )
+        #        logging.debug( "Key: {}, value {}".format(tag, tags[tag]) )
 
-    #Check tags has "EXIF DateTimeOriginal" attribute
-    if "EXIF DateTimeOriginal" not in tags :
-        print( "EXIF not found. aFilePath={}".format(aFilePath) )
+    for count in range( 1 ) :
+        if "EXIF DateTimeOriginal" in tags :
+            #2015:12:31 00:12:34
+            dateExif = datetime.datetime.strptime( tags["EXIF DateTimeOriginal"].printable , "%Y:%m:%d %H:%M:%S" )
+            if g_dateEarliest < dateExif and dateExif < datetime.datetime.now() :
+                dateFinal = dateExif
+                break
     else :
-        aryBaseNameDesc = g_reBaseNameDesc.match( aBaseName )
-        
-        #2015:12:31 00:12:34
-        aryTime = g_reExifTime.match( tags["EXIF DateTimeOriginal"].printable )
-        if ( aryTime ) :
-            #Merge original file name description
-            if ( aryBaseNameDesc ) :
-                strNewBaseName = "{}{}{}_{}{}{}-{}".format( aryTime.group(1) , aryTime.group(2) , aryTime.group(3) ,
-                                                            aryTime.group(4) , aryTime.group(5) , aryTime.group(6) , 
-                                                            aryBaseNameDesc.group(1) )
-            else :
-                strNewBaseName = "{}{}{}_{}{}{}".format( aryTime.group(1) , aryTime.group(2) , aryTime.group(3) ,
-                                                         aryTime.group(4) , aryTime.group(5) , aryTime.group(6) )
+        logging.debug( "EXIF not found. aFilePath={}".format(aFilePath) )
 
-            strNewFileName = GetNewFileName( aDirName , strNewBaseName , aExt )
-            bRet = True
-        else :
-            print( "EXIF format error. aFilePath={}, EXIF={}".format( aFilePath , tags["EXIF DateTimeOriginal"].printable ) )
-
-    if ( bRet ) :
-        print( "{} => {}".format(aFileName , strNewFileName) )
-        os.rename( aFilePath , aDirName + strNewFileName )
-    return bRet
+    return dateFinal
 
 
 
-def RenameByFileName( aFilePath , aDirName , aFileName , aBaseName , aExt ) :
-    bRet = False
+def GetTimeByFileName( aFilePath , aDirName , aFileName , aBaseName , aExt ) :
+    dateFinal = None
     
     for count in range( 1 ) :
-        #FB_IMG_1469184029530
-        aryFbTime = g_reFbTime.match( aBaseName )
-        if ( aryFbTime ) :
-            strTime = aryFbTime.group( 1 )
+        #My-Description-C360_2016-07-31-20-50-59-123-My-Description
+        aryDateTime = g_reDateTime.match( aBaseName )
+        if ( aryDateTime ) :
+            dateDateTime = datetime.datetime( year=(int)(aryDateTime.group(3)) , month=(int)(aryDateTime.group(4)) , day=(int)(aryDateTime.group(5)) , 
+                                              hour=(int)(aryDateTime.group(6)) , minute=(int)(aryDateTime.group(7)) , second=(int)(aryDateTime.group(8)) )
+            if g_dateEarliest < dateDateTime and dateDateTime < datetime.datetime.now() :
+                dateFinal = dateDateTime
+                break
+
+
+
+        #My-Description-FB_IMG_1469184029530-My-Description
+        aryTimeStamp = g_reTimeStamp.match( aBaseName )
+        if ( aryTimeStamp ) :
+            #Convert timestamp to specific format
+            strTime = aryTimeStamp.group( 3 )
             if ( len(strTime) > 10 ) :
                 strTime = strTime[:10]
-            strNewBaseName = datetime.datetime.fromtimestamp( int(strTime) ).strftime( "%Y%m%d_%H%M%S" )
-            strNewFileName = GetNewFileName( aDirName , strNewBaseName , aExt )
-            bRet = True
-            break
-        
-        #Screenshot_2016-07-31-20-50-59
-        aryScreenshotTime = g_reScreenshotTime.match( aBaseName )
-        if ( aryScreenshotTime ) :
-            strNewBaseName = "{}{}{}_{}{}{}".format( aryScreenshotTime.group(1) , aryScreenshotTime.group(2) , aryScreenshotTime.group(3) ,
-                                                     aryScreenshotTime.group(4) , aryScreenshotTime.group(5) , aryScreenshotTime.group(6) )
-            strNewFileName = GetNewFileName( aDirName , strNewBaseName , aExt )
-            bRet = True
-            break
+            dateTimeStamp = datetime.datetime.fromtimestamp( int(strTime) )
+            if g_dateEarliest < dateTimeStamp and dateTimeStamp < datetime.datetime.now() :
+                dateFinal = dateTimeStamp
+                break
     else :
-        print( "Unknown filename format. aFilePath={}".format( aFilePath ) )
+        logging.debug( "Time not found in filename. aFilePath={}".format(aFilePath) )
 
-    if ( bRet ) :
-        print( "{} => {}".format(aFileName , strNewFileName) )
-        os.rename( aFilePath , aDirName + strNewFileName )
-    return bRet
+    return dateFinal
+
+
+
+def GetTimeByMediaInfo( aFilePath , aDirName , aFileName , aBaseName , aExt ) :
+    dateNow = datetime.datetime.now()
+    dateFinal = dateNow
+
+    info = MediaInfo.parse( aFilePath )
+    logging.debug( info.to_json() )
+
+    for track in info.tracks :
+        if track.track_type == "General" :
+            dateEncoded = datetime.datetime.now()
+            dateCreation = datetime.datetime.now()
+            dateModification = datetime.datetime.now()
+            if track.encoded_date :
+                dateEncoded = datetime.datetime.strptime( track.encoded_date , "UTC %Y-%m-%d %H:%M:%S" ) + ( datetime.datetime.now() - datetime.datetime.utcnow() )
+                if track.duration :
+                    dateEncoded = dateEncoded - datetime.timedelta( milliseconds=track.duration )
+                if g_dateEarliest < dateEncoded and dateEncoded < dateFinal :
+                    dateFinal = dateEncoded
+            if track.file_creation_date__local :
+                dateCreation = datetime.datetime.strptime( track.file_creation_date__local , "%Y-%m-%d %H:%M:%S.%f" )
+                if g_dateEarliest < dateCreation and dateCreation < dateFinal :
+                    dateFinal = dateCreation
+            if track.file_last_modification_date__local :
+                dateModification = datetime.datetime.strptime( track.file_last_modification_date__local , "%Y-%m-%d %H:%M:%S.%f" )
+                if g_dateEarliest < dateModification and dateModification < dateFinal :
+                    dateFinal = dateModification
+            
+            if dateFinal != dateNow :
+                break
+    else :
+        logging.warning( "MediaInfo not found. aFilePath={}".format(aFilePath) )
+    
+    return dateFinal
+
 
 
 
 if __name__ == "__main__" :
-    reFilter = re.compile( ".*\.(jpg|png)$" , re.IGNORECASE )
-    strPath = os.getcwd()
-    if ( 2 <= len(sys.argv) ) :
-        strPath = sys.argv[1]
+    strScriptDir = os.path.dirname( os.path.realpath(__file__) )
+    strLogDir = "{}\\Logs".format( strScriptDir )
+    if not os.path.isdir( strLogDir ) :
+        os.makedirs( strLogDir )
 
+    strPath = os.path.realpath( os.getcwd() )
+    if ( 2 <= len(sys.argv) ) :
+        strPath = os.path.realpath( sys.argv[1] )
+
+    logger = logging.getLogger()
+    logger.setLevel( logging.INFO )
+
+    fmtConsole = logging.Formatter( "[%(asctime)s][%(levelname)s]: %(message)s" )
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter( fmtConsole )
+    logger.addHandler( consoleHandler )
+
+    fmtFile = logging.Formatter( "[%(asctime)s][%(levelname)s][%(process)04X:%(thread)04X][%(filename)s][%(funcName)s_%(lineno)d]: %(message)s" )
+    fileHandler = logging.FileHandler( "{}\\{}-{}.txt".format(strLogDir , os.path.basename(strPath) , datetime.datetime.now().strftime("%Y%m%d_%H%M%S")) )
+    fileHandler.setFormatter( fmtFile )
+    logger.addHandler( fileHandler )
+
+
+
+    reFilter = re.compile( ".*\.(bmp|jpg|png|mp3|mp4|m4a|amr)$" , re.IGNORECASE )
     try :
-        print( "Search {} under \"{}\"".format(reFilter,strPath) )
+        #Add _Tools directory to %PATH%
+        if os.environ["PATH"].find( "MediaInfo" ) == -1 :
+            strToolDir = "{}\\_Tools\\".format( strScriptDir )
+            os.environ["PATH"] = strToolDir + ";" + os.environ["PATH"]
+
+        logging.info( "Search {} under \"{}\"".format(reFilter,strPath) )
         if ( os.path.isfile(strPath) ) :
             if reFilter.match( strPath ) :
                 strDir , strFileName = os.path.split( strPath )
@@ -146,10 +258,11 @@ if __name__ == "__main__" :
                             strDir += "\\"
                         TryRenameFile( strPath , strDir , strFileName , strBaseName , strExt )
         else :
-            print( "Usage: {} <file or directory path>".format( os.path.basename( sys.argv[0] ) ) )
+            logging.info( "Usage: {} <file or directory path>".format( os.path.basename( sys.argv[0] ) ) )
     except Exception as ex :
-        print( "Error: {}".format(ex) )
+        logging.exception( "strPath={}".format(strPath) )
 
-    print( "End of the program" )
+    logging.info( "End of the program" )
+    logging.shutdown()
     print( "Press any key to leave" )
     input()
